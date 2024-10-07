48ee4965d6f6b1f9deedbf8042f59d5ee06175ff
Xeplosion <xeplosion@xegames.online>
2024-10-06 23:51:25 -0600

    Added functionality to the cat, tac, and nl functions.

--- src/Concatenate.py
+++ src/Concatenate.py
@@ -19,16 +19,51 @@
 #                                 naturally sentient (or close enough).
 
 
-def cat(args):
+def cat(args, usage_callback):
     """concatenate files and print on the standard output"""
-    print("TODO: concatenate files and print on the standard output")
+    # Check for valid number of arguments
+    if len(args) < 1:
+        usage_callback(error="Error: too few arguments", tool="cat")
+        return
+
+    # Concatenate the files
+    for filename in args:
+        with open(filename, 'r') as file:
+            for line in file:
+                print(line, end='')tac
 
 
-def tac(args):
+def tac(args, usage_callback):
     """concatenate and print files in reverse"""
-    print("TODO: concatenate and print files in reverse")
+    # Check for valid number of arguments
+    if len(args) < 1:
+        usage_callback(error="Error: too few arguments", tool="tac")
+        return
+
+    # Concatenate the files
+    for filename in args:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for line in reversed(lines):
+                print(line, end='')
 
 
-def nl(args):
+def nl(args, usage_callback):
     """number lines of files"""
-    print("TODO: number lines of files")
+    # Check for valid number of arguments
+    if len(args) < 1:
+        usage_callback(error="Error: too few arguments", tool="nl")
+        return
+
+    # Concatenate the files
+    line_number = 1
+    for filename in args:
+        with open(filename, 'r') as file:
+            for line in file:
+                # If the line is not blank
+                if line.strip():
+                    print(f"{line_number} {line}", end='')
+                    line_number += 1
+                    continue
+
+                print(line, end='')
