b71cd928ae2afc5b0bd4b5c478847321d2f4f1f4
Xeplosion <xeplosion@xegames.online>
2024-10-06 23:52:20 -0600

    Completed the sort function  TODO.

--- src/Partial.py
+++ src/Partial.py
@@ -19,11 +19,76 @@
 #                                 naturally sentient (or close enough).
 
 
-def head(args):
+def head(args, usage_callback):
     """output the first part of files"""
-    print("TODO: output the first part of files")
+    if len(args) < 1:
+        usage_callback(error="Error: too few arguments", tool="head")
+    # Parse flags
+    n = 10
+    if '-n' in args:
+        try:
+            n_index = args.index('-n')
+            n = int(args[n_index + 1])
+            args = args[:n_index] + args[n_index + 2:]  # Isolate the filepath arguments
+            del args[n_index:n_index + 1]
+        except (ValueError, IndexError):
+            usage_callback(error="Error: number of lines is required", tool="head")
+            return
+
+    # Concatenate the files
+    multiple = False
+    if len(args) > 1:
+        multiple = True
+
+    for filename in args:
+        if multiple:
+            print(f"==> {filename} <==")
+
+        with open(filename, 'r') as file:
+            line_count = 1
+            for line in file:
+                if line_count > n:
+                    break
+
+                print(line, end='')
+                line_count += 1
+
+        print(' ')
 
 
-def tail(args):
+def tail(args, usage_callback):
     """output the last part of files"""
-    print("TODO: output the last part of files")
+    if len(args) < 1:
+        usage_callback(error="Error: too few arguments", tool="head")
+    # Parse flags
+    n = 10
+    if '-n' in args:
+        try:
+            n_index = args.index('-n')
+            n = int(args[n_index + 1])
+            args = args[:n_index] + args[n_index + 2:]  # Isolate the filepath arguments
+            del args[n_index:n_index + 1]
+        except (ValueError, IndexError):
+            usage_callback(error="Error: invalid field specification", tool="head")
+            return
+
+    # Concatenate the files
+    multiple = False
+    if len(args) > 1:
+        multiple = True
+
+    for filename in args:
+        if multiple:
+            print(f"==> {filename} <==")
+
+        with open(filename, 'r') as file:
+            line_count = 1
+            lines = file.readlines()
+            for line in lines[-n:]:
+                if line_count > n:
+                    break
+
+                print(line, end='')
+                line_count += 1
+
+        print('')
